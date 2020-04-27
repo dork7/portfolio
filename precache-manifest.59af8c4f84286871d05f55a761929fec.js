d25ad6759574e6628f7be048e007664ff8b41374
dork7 <hamzameh122@gmail.com>
2020-04-27 23:56:42 +0500

    Updates

---
+++ precache-manifest.59af8c4f84286871d05f55a761929fec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efae452d8956bb292bd312be811e7632",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1ffcff45be7f4049f66e",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1223183176975513d517",
+    "url": "/portfolio/static/css/main.5887a089.chunk.css"
+  },
+  {
+    "revision": "1ffcff45be7f4049f66e",
+    "url": "/portfolio/static/js/2.734373df.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.734373df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1223183176975513d517",
+    "url": "/portfolio/static/js/main.40911570.chunk.js"
+  },
+  {
+    "revision": "7e84dee687ac6de54091",
+    "url": "/portfolio/static/js/runtime-main.4da689ad.js"
+  },
+  {
+    "revision": "2e0f308439a6bf144d53bc25f7f7151f",
+    "url": "/portfolio/static/media/img.2e0f3084.jpg"
+  }
+]);
