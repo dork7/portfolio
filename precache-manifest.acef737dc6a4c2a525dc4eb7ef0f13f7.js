e3e6fe579b8f1f9fd744d31ef171c9be44adfe2e
dork7 <hamzameh122@gmail.com>
2020-04-27 20:17:02 +0500

    Updates

---
+++ precache-manifest.acef737dc6a4c2a525dc4eb7ef0f13f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34800f2f9623ee41daaed48d200a6707",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1ffcff45be7f4049f66e",
+    "url": "/react-deploy/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "50f8610d94421c6dcea5",
+    "url": "/react-deploy/static/css/main.a838592e.chunk.css"
+  },
+  {
+    "revision": "1ffcff45be7f4049f66e",
+    "url": "/react-deploy/static/js/2.734373df.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-deploy/static/js/2.734373df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f8610d94421c6dcea5",
+    "url": "/react-deploy/static/js/main.d7d8d0dc.chunk.js"
+  },
+  {
+    "revision": "b367fc49837165f21248",
+    "url": "/react-deploy/static/js/runtime-main.95d24a82.js"
+  },
+  {
+    "revision": "2e0f308439a6bf144d53bc25f7f7151f",
+    "url": "/react-deploy/static/media/img.2e0f3084.jpg"
+  }
+]);
